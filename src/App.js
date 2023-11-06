a684524393b9907890f7861c3796991841cdc6c0
Pavel <mr.osoker@yandex.ru>
2023-11-06 23:47:14 +0600

    remove useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
